f97ffcaec19dc7d195f45b2c84846eb256bb0d00
gozali <fikri.gozali14@gmail.com>
2023-01-24 08:32:55 +0700

    add search bar

--- js/script.js
+++ js/script.js
@@ -3,15 +3,27 @@ feather.replace();
 // navbar menu
 const navbarNav = document.querySelector(".navbar-nav");
 const hamMenu = document.querySelector("#hamburger-menu");
+const searchBar = document.querySelector(".search-bar");
+const searchBox = document.querySelector(".search-box");
 
 // navbar click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar navbar untunk emngilakan nav
+document.querySelector(".search-box").onclick = () => {
+  searchBar.classList.add("view");
+};
+
+// klik di luar navbar untuk menghilakan nav
 document.addEventListener("click", function (e) {
   if (!hamMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!searchBox.contains(e.target)) {
+    searchBar.classList.remove("view");
   }
 });
